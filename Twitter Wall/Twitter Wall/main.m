9901597c45a5b70806c764e2e68cd4b4793d1ca6
Nick Meinhold <nickmeinhold@Nicks-MacBook-Pro-2.local>
2013-02-15 11:47:25 +1000

    Committing empty Project.

---
+++ Twitter Wall/Twitter Wall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twitter Wall
+//
+//  Created by Nick Meinhold on 15/02/13.
+//  Copyright (c) 2013 NPENPI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "npenpiAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([npenpiAppDelegate class]));
+    }
+}
---
+++ Twitter Wall/Twitter Wall/npenpiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  npenpiAppDelegate.h
+//  Twitter Wall
+//
+//  Created by Nick Meinhold on 15/02/13.
+//  Copyright (c) 2013 NPENPI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface npenpiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
